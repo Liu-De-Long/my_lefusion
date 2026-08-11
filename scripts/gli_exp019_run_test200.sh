f8a6fa345c3881bc26a3d67bd9827afc81b02cec
pc <jinyuanbao739@outlook.com>
2026-08-12 00:31:09 +0800

    perf(exp019): raise inference batch to eight

--- scripts/gli_exp019_run_test200.sh
+++ scripts/gli_exp019_run_test200.sh
@@ -54,15 +54,15 @@ log_phase "formal_start_commit_$(git -C "$REPO" rev-parse HEAD)"
 
 log_phase preflight_start
 "$PYTHON" "$INFER" --config-name gli_exp019_exp010_test200_shard0 \
-  output.root="$OUT/preflight_batch4/exp010" output.max_batches=1 >"$OUT/preflight_batch4_exp010.log" 2>&1 &
+  output.root="$OUT/preflight_batch8/exp010" output.max_batches=1 >"$OUT/preflight_batch8_exp010.log" 2>&1 &
 preflight0=$!
 "$PYTHON" "$INFER" --config-name gli_exp019_direct_test200_shard1 \
-  output.root="$OUT/preflight_batch4/direct" output.max_batches=1 >"$OUT/preflight_batch4_direct.log" 2>&1 &
+  output.root="$OUT/preflight_batch8/direct" output.max_batches=1 >"$OUT/preflight_batch8_direct.log" 2>&1 &
 preflight1=$!
 wait "$preflight0"
 wait "$preflight1"
 "$PYTHON" "$INFER" --config-name gli_exp019_filtered_test200_shard0 \
-  output.root="$OUT/preflight_batch4/filtered" output.max_batches=1 >"$OUT/preflight_batch4_filtered.log" 2>&1
+  output.root="$OUT/preflight_batch8/filtered" output.max_batches=1 >"$OUT/preflight_batch8_filtered.log" 2>&1
 log_phase "preflight_complete_elapsed_$(elapsed)s"
 
 run_pair exp010 gli_exp019_exp010_test200_shard0 gli_exp019_exp010_test200_shard1
