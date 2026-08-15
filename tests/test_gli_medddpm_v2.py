02c86aa519677a0834ae7944708c1949db6421ee
pc <jinyuanbao739@outlook.com>
2026-08-15 08:18:51 +0800

    test(exp023): use dependency-free Med-DDPM unit tests

--- tests/test_gli_medddpm_v2.py
+++ tests/test_gli_medddpm_v2.py
@@ -1,4 +1,5 @@
 import importlib.util
+import unittest
 from pathlib import Path
 
 import numpy as np
@@ -10,16 +11,20 @@ MODULE = importlib.util.module_from_spec(SPEC)
 SPEC.loader.exec_module(MODULE)
 
 
-def test_xyz_to_cdhw_preserves_voxel_order():
-    xyz = np.arange(64 * 64 * 32, dtype=np.float32).reshape(64, 64, 32)
-    cdhw = MODULE.xyz_to_cdhw(xyz)
-    assert cdhw.shape == (1, 32, 64, 64)
-    assert cdhw[0, 7, 11, 13] == xyz[11, 13, 7]
+class MedDDPMV2Test(unittest.TestCase):
+    def test_xyz_to_cdhw_preserves_voxel_order(self):
+        xyz = np.arange(64 * 64 * 32, dtype=np.float32).reshape(64, 64, 32)
+        cdhw = MODULE.xyz_to_cdhw(xyz)
+        self.assertEqual(cdhw.shape, (1, 32, 64, 64))
+        self.assertEqual(cdhw[0, 7, 11, 13], xyz[11, 13, 7])
+
+    def test_state_dict_prefix_normalization(self):
+        value = MODULE.torch.ones(1)
+        single = MODULE.normalize_state_dict({"denoise_fn.module.x": value}, multi_gpu=False)
+        self.assertEqual(set(single), {"denoise_fn.x"})
+        multi = MODULE.normalize_state_dict({"denoise_fn.x": value}, multi_gpu=True)
+        self.assertEqual(set(multi), {"denoise_fn.module.x"})
 
 
-def test_state_dict_prefix_normalization():
-    value = MODULE.torch.ones(1)
-    single = MODULE.normalize_state_dict({"denoise_fn.module.x": value}, multi_gpu=False)
-    assert set(single) == {"denoise_fn.x"}
-    multi = MODULE.normalize_state_dict({"denoise_fn.x": value}, multi_gpu=True)
-    assert set(multi) == {"denoise_fn.module.x"}
+if __name__ == "__main__":
+    unittest.main()
